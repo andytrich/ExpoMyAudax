871466b829c54d3a6fea36a6016d6200261a46d5
Andy Rich <ARich@independentvetcare.co.uk>
2019-09-19 15:16:11 +0100

    Tidied up home page

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { NavigationInjectedProps } from 'react-navigation';
-import { Col, Grid } from 'react-native-easy-grid';
-import { Container, Card, CardItem, Content, Text } from 'native-base';
+import { Container, Card, CardItem, Content, Text, View } from 'native-base';
+import { Avatar, Icon } from 'react-native-elements';
 import FooterComponent from './Footer';
 import HeaderComponent from './Header';
 
@@ -22,23 +22,19 @@ export default class HomeComponent extends React.Component<NavigationInjectedPro
     return (
       <Container style={{marginTop:25}}>
         <HeaderComponent></HeaderComponent>
-        <Content>
-          <Grid>
-            <Col>
-            <Card>
-              <CardItem cardBody button onPress={()=>{this.props.navigation.navigate('EventRides')}}>
-                <Text>Search for a ride</Text>
+        <Content style={{marginTop:150}}>
+            <Card >
+              <CardItem style={{width:'100%', justifyContent:'center'}} cardBody button onPress={()=>{this.props.navigation.navigate('EventRides')}}>
+              <Icon name='search' size={100}></Icon>
+              <Text>Search for a ride</Text>
               </CardItem>
             </Card>
-            </Col>
-            <Col>
-            <Card>
-              <CardItem cardBody button onPress={()=>{this.props.navigation.navigate('AuthLoading')}}>
-                <Text>Members</Text>
+            <Card >
+              <CardItem style={{width:'100%', justifyContent:'center'}} cardBody button onPress={()=>{this.props.navigation.navigate('AuthLoading')}}>
+              <Icon name='bicycle' type='font-awesome' size={100} />
+              <Text>Members</Text>
               </CardItem>
             </Card>
-            </Col>
-          </Grid>
         </Content>
         <FooterComponent navigation={this.props.navigation}></FooterComponent>
       </Container>
