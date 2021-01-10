9be649071270bb25e0219e23d5a30ddc8b7e92d4
Andy Rich <Andy.Rich@ivcevidensia.com>
2021-01-10 12:35:28 +0000

    Header graphics

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View} from 'react-native'
+import {Image, Text, View} from 'react-native'
 
 export function AppHeader() {  
   const dataArray = [
@@ -8,9 +8,16 @@ export function AppHeader() {
   { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
 ];
   return (
-    <View style={{flexDirection: 'row', justifyContent: 'space-between', marginLeft: 25, marginRight: 25, marginBottom: 25} }>
-      <Text>Header</Text>
-      <Text>|||</Text>
+    <View style={{marginLeft: 25, marginRight: 25, marginBottom: 25}}>
+    <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingBottom:15 }}>
+      <Image style={{ height: 50, width: 100 }} source={require('../assets/audax-logo.png')} />
+      <Image style={{height:50, width:120}} source={require('../assets/DropDown.png')} />
+      <Image style={{ height: 50, width: 70 }} source={require('../assets/Menu.png')} />
+
+      </View>        
+      <View style={{ flexDirection: 'row' }}>
+        <Image style={{ height: 1, width:'100%', alignSelf:'stretch'}} source={require('../assets/Divider.png')} />
+        </View>
       </View>
-);
+  );
 };
